271de3e586d318033e0ff6d8f29d786502779f42
Phoenix-just <54228062+Phoenix-just@users.noreply.github.com>
2025-05-01 17:02:20 +0530

    Delete client directory

--- client/src/components/RemoteVideoPlayer.js
+++
@@ -1,9 +0,0 @@
-const RemoteVideoPlayer = (inputStream) => {
-    return (
-        <div>
-            <h1>Remote Video Player</h1>
-            <video id="localVideoPlayer" width="640" height="480" />
-        </div>
-    )
-}  
-export default RemoteVideoPlayer;
